85df5e24eddb06b67664f8c869e3df9df659ae79
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-02-26 21:19:25 +0300

    First Commit

---
+++ FileStream/FileOperation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOperation
+{
+    public static class FileOperation
+    {
+        public static void WriteToFile(string text)
+        {
+            string path = @"C:\\Users\\HasanBerkErgun\\Desktop\\FolderTest\\test.txt";
+            if(!File.Exists(path))
+            {
+                File.Create(path).Close();
+                using (StreamWriter streamWriter = File.CreateText(path))
+                    streamWriter.WriteLine(text);
+               
+            }
+            else
+            {
+                using (StreamWriter streamWriter = File.AppendText(path))
+                    streamWriter.WriteLine(text);                
+            }
+
+        }
+
+        public static string ReadFromFile()
+        {
+            string path = @"C:\\Users\\HasanBerkErgun\\Desktop\\FolderTest\\test.txt";
+            string text = File.ReadAllText(path); //Or Use Other Method
+            return text;
+        }
+    }
+}
---
+++ FileStream/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileOperation.WriteToFile("Ekleme"); 
+            FileOperation.WriteToFile("Yaptım");
+            Console.WriteLine("Successfully Written To File");
+            Console.WriteLine(FileOperation.ReadFromFile());
+            Console.WriteLine("Read From File Successfully");
+            Console.ReadKey();
+
+        }
+    }
+}
